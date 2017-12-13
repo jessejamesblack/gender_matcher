dc992f8b66c2752844e65e1586dd2e247effb5f1
dchen <chendavid96@gmail.com>
2017-12-12 23:35:32 -0500

    image set to binary

--- classifier.py
+++ classifier.py
@@ -1,25 +1,45 @@
 import cv2
 
-from modules.tools import show_image_until_keypress
+from modules.tools import show_image_until_keypress, make_img_black_and_white
 
 IMAGE_PATH = 'faces/Face 44.png'
 
 def main():
     # Read in image with cv2
-    original = cv2.imread(IMAGE_PATH)
-    gray_image = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-    # contrast = increase_image_contrast(gray_image)
-    show_image_until_keypress(gray_image)
+    img = cv2.imread(IMAGE_PATH)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = make_img_black_and_white(img_gray)
+    show_image_until_keypress(img)
+    show_image_until_keypress(img_gray)
 
-    img_rows, img_cols = gray_image.shape
+    num_rows, num_cols = img.shape
 
-    print(img_rows, img_cols)
+    top_left = img[0, 0]
+    top_right = img[0, num_cols - 1]
+    bottom_left = img[num_rows - 1, 0]
+    bottom_right = img[num_rows - 1, num_cols - 1]
+
+    # print('top_left = {}'.format(top_left))
+    # print('top_right = {}'.format(top_right))
+    # print('bottom_left = {}'.format(bottom_left))
+    # print('bottom_right = {}'.format(bottom_right))
+    print('number of rows: {}'.format(num_rows))
+    print('number of cols: {}'.format(num_cols))
+
+    for col in range(num_cols):
+
+        col_sum = 0
+
+        for row in range(num_rows):
+            col_sum += img[row, col]
+
+        print('avg of col {}: {}'.format(col + 1, col_sum / num_rows))
+
 
     # for row in img_rows:
     #     print(row)
 
     # trimmed_img = trim_image_padding(img)
-
 
 
 if __name__ == '__main__':
--- modules/tools.py
+++ modules/tools.py
@@ -12,4 +12,7 @@ def show_image_until_keypress(image):
     cv2.destroyAllWindows()
 
 
-def increase_image_contrast(image):
+def make_img_black_and_white(image):
+    (thresh, im_bw) = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    return im_bw
