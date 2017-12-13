30e4923a93b1cf16712567d91568fb9370017288
dchen <chendavid96@gmail.com>
2017-12-12 21:33:24 -0500

    started img processing

--- classifier.py
+++ classifier.py
@@ -1,13 +1,18 @@
+import cv2
+
 from modules.tools import trim_image_padding
 
+IMAGE_PATH = 'faces/Face 1.png'
+
 def main():
-
-
-
+    # Read in image with cv2
+    img = cv2.imread(IMAGE_PATH)
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     # First, process the image so that extra white space is removed.
-
-    trimmed_img = trim_image_padding(image)
+    trimmed_img = trim_image_padding(img)
 
 
 
